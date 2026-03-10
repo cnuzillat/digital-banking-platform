a4db606ef9cc08c8c5d72049ffe7e83674d3fd06
cnuzillat <cnuzillat@gmail.com>
2026-03-09 21:59:12 -0400

    Initial Spring Boot project generated with Spring Initializr

---
+++ src/main/java/com/chloe/digital_banking_platform/DigitalBankingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.chloe.digital_banking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chloe/digital_banking_platform/DigitalBankingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloe.digital_banking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
